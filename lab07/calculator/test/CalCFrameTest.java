20fc823d04e9db73ced04b0779859ed39694cd31
jiachuhao123 <79613405+jiachuhao123@users.noreply.github.com>
2021-07-18 18:56:44 +0800

    Add files via upload

---
+++ lab07/calculator/test/CalCFrameTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
+class CalcFrameTest {
+
+  @Test
+  void do_nothing() {
+  	assertEquals(1, 1);
+  }
+
+}
